649d115052eab87fdc8e819eb3dfc204d4665b24
leetom <leetom1991@163.com>
2014-02-19 21:04:37 +0800

    modify files

--- lib/MoQa.pm
+++ lib/MoQa.pm
@@ -8,10 +8,11 @@ use DBI;
 #connect to db
 #
 my $pass = `mypass`;
-print $pass;
 our $DB = DBI->connect("DBI:mysql:database=moqa;host=localhost", "root", $pass, 
     {'RaiseError' => 1}
 );
+
+$DB->do("SET CHARSET UTF8");
 
 
 sub startup {
@@ -33,6 +34,9 @@ sub startup {
 
   $r->get('/user/login')->to('user#login');
   $r->post('/user/login')->to('user#check');
+
+  $r->get('/user/reg')->to('user#reg');
+  $r->post('/user/reg')->to('user#save');
 
   $r->get('/user/logout')->to('user#logout');
 }
